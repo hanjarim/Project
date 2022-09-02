d703b6e1e1c398a74e52180a00fc8f5553befd54
Timothy Matara <tondieki@safaricom.co.ke>
2022-09-02 16:13:20 +0300

    add dashboard view

--- complaintapp/urls.py
+++ complaintapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (
     CustomerComplaint,
+    Dashboard
 )
 
 app_name = 'complaintapp'
 
 urlpatterns = [
     path('', CustomerComplaint, name='complaintform'),
+    path('dashboard/', Dashboard, name='dashboard'),
 ]
--- complaintapp/views.py
+++ complaintapp/views.py
@@ -1,10 +1,53 @@
 from django.shortcuts import render
 from complaintapp.forms import ComplaintCaptureForm
+from complaintapp.models import (
+    Complaints, 
+    Locations,
+    Tasks
+)
 
 def CustomerComplaint(request):
     form = ComplaintCaptureForm(request.POST or None)
+
+    if request.method == 'POST':
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        location = request.POST.get('location')
+        landmark = request.POST.get('landmark')
+        coordinates = request.POST.get('coordinates')
+        category = request.POST.get('category')
+        details = request.POST.get('details')
+
+        # Get location
+        location_qs = Locations.objects.get(pk=location)
+
+        # Add a new record
+        Complaints.objects.create(
+            phone=phone,
+            email=email,
+            location=location_qs,
+            landmark=landmark,
+            category=category,
+            details=details,
+            coordinates=coordinates,
+            status='open'
+        )
+
+
     context = {
         'form': form
     }
 
     return render(request, 'complaintapp/complaint.html', context)
+
+
+def Dashboard(request):
+    print(request.user)
+    # List all open complaints
+    ticket_qs = Complaints.objects.all()
+
+    context = {
+        'complaints': ticket_qs
+    }
+
+    return render(request, 'complaintapp/dashboard.html', context)
